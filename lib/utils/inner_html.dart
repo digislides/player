e7c3e5c3d75e9d3b4923d29ce4bb79d7f996b739
tejainece <tejainece@gmail.com>
2019-02-21 17:32:33 +0100

    Moved ClockComponent to wclient

--- lib/utils/inner_html.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:html';
-
-class MyNodeValidator implements NodeValidator {
-  bool allowsElement(Element element) => true;
-
-  bool allowsAttribute(Element element, String attributeName, String value) =>
-      true;
-}
