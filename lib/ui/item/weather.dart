6829ecf397aa09f32b75ae5a46bd4c2506bf05ef
tejainece <tejainece@gmail.com>
2019-03-09 14:54:53 +0100

    Weather item

--- lib/ui/item/weather.dart
+++ lib/ui/item/weather.dart
@@ -12,13 +12,36 @@ class WeatherItemView implements PageItemView {
     _build();
   }
 
+  final _icon = DivElement();
+
   void _build() {
+    root.classes
+        .addAll(['page-item', 'page-item-weather', 'page-item-${item.id}']);
+
+    root.style.width = '${item.width}px';
+    root.style.height = '${item.height}px';
+
+    root.style.left = '${item.left}px';
+    root.style.top = '${item.top}px';
+
+    _icon.style.width = "100%";
+    _icon.style.height = "100%";
+
+    _icon.classes.addAll([item.theme.css]);
+    _icon.classes.add(item.dummy.css);
+
+    _icon.style.backgroundColor = item.color;
+
+    root.children.add(_icon);
+  }
+
+  @override
+  void play() {
     // TODO
   }
 
   @override
-  void play() {}
-
-  @override
-  void stop() {}
+  void stop() {
+    // TODO
+  }
 }
